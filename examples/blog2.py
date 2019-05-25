549bb29d04905c948590587e953c188e59d41c79
shaneliuyx <shane_liuyx@hotmail.com>
2019-05-25 21:42:52 +0800

    add examples

--- examples/blog2.py
+++ examples/blog2.py
@@ -1,15 +1,14 @@
 from select_ri import *
 import pandas as pd
-
-input = pd.read_excel("blog2_input.xlsx")
+#os.environ['EXCLUDE_EC2_TYPE'] = "5"
+input = pd.read_excel("blog2_input_simple.xlsx")
 output = pd.DataFrame()
 
 ri = RI()
-for i in range(0, input.shape[0]):
-    row = pd.DataFrame(input.loc[[i]])
+for index, row in input.iterrows():
     row = row.reset_index(drop=True)
     result = ri.select_ec2_by_config(input_row=row)
     output = output.append(result, ignore_index=True, sort=False)
 output = pd.concat([input, output], axis=1, join_axes=[input.index])
 print (output)
-output.to_excel('blog2_output.xlsx', index=False)
+output.to_excel('blog2_output_simple.xlsx', index=False)
--- examples/blog3.py
+++ examples/blog3.py
@@ -3,7 +3,7 @@ from select_ri import *
 #from select_ri_no5_sap import *
 import pandas as pd
 
-input = pd.read_excel("blog3_input.xlsx")
+input = pd.read_excel("blog5_input.xlsx")
 output = pd.DataFrame()
 #os.environ['EXCLUDE_EC2_TYPE'] = "3,t"
 
@@ -15,4 +15,4 @@ for i in range(0, input.shape[0]):
     output = output.append(result, ignore_index=True, sort=False)
 output = pd.concat([input, output], axis=1, join_axes=[input.index])
 print (output)
-output.to_excel('blog3_output.xlsx', index=False)
+output.to_excel('blog5_output.xlsx', index=False)
